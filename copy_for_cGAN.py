79d6ef36eda66726d4036362d0f3f475a78ea96d
Kevin <jinc0008@e.ntu.edu.sg>
2020-11-26 14:05:43 +0800

    test

--- copy_for_cGAN.py
+++ copy_for_cGAN.py
@@ -7,6 +7,8 @@ a = 'Train Test Splitting list/normal_training/NTU_train_correct.txt'
 b = 'new_split_list/train.txt'
 
 def copy(f,des):
+    if not os.path.exists(des):
+        os.mkdir(des)
     folder,name = [],[]
     with open(f) as f:
         lines = f.read().splitlines()
@@ -19,9 +21,11 @@ def copy(f,des):
 
     for i in range(len(folder)):
         path = os.path.join(folder[i],'pngs_544_960',name[i]+'.png')
-        copyfile(path,des+name[i]+'.png')
+        tar = os.path.join(des,name[i]+'.png')
+        copyfile(path,tar)
+        print('copied',path,'to',tar)
 
-copy(a,'trainA/')
-copy(b,'trainB/')
+copy(a,'trainA')
+copy(b,'trainB')
 
 
